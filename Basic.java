3e91ac2f0637b684656fd1c7c4fd126abdc05ebf
KEROLOS AYMAN EBRAHIM ABDELMALEK <149635997+keorls209@users.noreply.github.com>
2024-12-08 00:38:37 +0200

    Update Basic.java

--- Basic.java
+++ Basic.java
@@ -3,7 +3,7 @@ public class Basic extends Plan {
 
 
     public Basic() {
-        super( "basic", 5, 10.0, "SD", 1, 0, "Limited access to content");
+        super( "basic", 5, 10.0, "SD", 5, 0, "Limited access to content");
     }
 
 
