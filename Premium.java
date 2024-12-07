61b85d467152e9a61b7102166ca0c1f647b471a2
KEROLOS AYMAN EBRAHIM ABDELMALEK <149635997+keorls209@users.noreply.github.com>
2024-12-08 00:37:50 +0200

    Update Premium.java

--- Premium.java
+++ Premium.java
@@ -2,7 +2,7 @@ public class Premium extends Plan
 {
 public Premium()
 {
-    super("premium" ,30, 50.0, "4k", Integer.MAX_VALUE, 4, "Full library access, early content access, priority support");
+    super("premium" ,30, 50.0, "4k", 50, 4, "Full library access, early content access, priority support");
 
 }
 
