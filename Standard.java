322a29696140e9eb807c44450f1a8fd13641a43e
KEROLOS AYMAN EBRAHIM ABDELMALEK <149635997+keorls209@users.noreply.github.com>
2024-12-08 00:38:22 +0200

    Update Standard.java

--- Standard.java
+++ Standard.java
@@ -2,7 +2,7 @@ public class Standard extends Plan
 {
 
     public Standard() {
-        super("standard",10, 20.0, "HD", 2, 2, "Access to a larger library, Get 10% off every third movie");
+        super("standard",10, 20.0, "HD", 10, 2, "Access to a larger library, Get 10% off every third movie");
     }
 }
 
