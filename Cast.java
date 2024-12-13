335e6cdc0af8aab92501e0b845aef457c1744cc6
mohamedyasser227 <mohamedyasserabbas@gmail.com>
2024-12-13 16:00:41 +0200

    Update Cast.java

--- Cast.java
+++ Cast.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class Cast {
     private String firstName;
@@ -6,79 +6,61 @@ public class Cast {
     private String dateOfBirth;
     private String gender;
     private String nationality;
-    private ArrayList<String> movies;
-    private ArrayList<String> awards;
-    private ArrayList<String> socialMediaLinks;
+    private List<String> movies;
+    private List<String> awards;
+    private List<String> socialMediaLinks;
 
-    
-    public Cast(String firstName, String lastName, String dateOfBirth, String gender, String nationality) {
+   
+    public Cast(String firstName, String lastName, String dateOfBirth, String gender, String nationality,
+                List<String> movies, List<String> awards, List<String> socialMediaLinks) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.dateOfBirth = dateOfBirth;
         this.gender = gender;
         this.nationality = nationality;
-        this.movies = new ArrayList<>();
-        this.awards = new ArrayList<>();
-        this.socialMediaLinks = new ArrayList<>();
-    }
-
-   
-    public void addMovie(String movie) {
-        movies.add(movie);
-    }
-
-   
-    public void displayMovies() {
-        System.out.println("Movies:");
-        for (String movie : movies) {
-            System.out.println("- " + movie);
-        }
-    }
-
-    
-    public void addAward(String award) {
-        awards.add(award);
-    }
-
-   
-    public void displayAwards() {
-        System.out.println("Awards:");
-        for (String award : awards) {
-            System.out.println("- " + award);
-        }
-    }
-
-    
-    public void addSocialMediaLink(String link) {
-        socialMediaLinks.add(link);
-    }
-
-    
-    public void displaySocialMediaLinks() {
-        System.out.println("Social Media Links:");
-        for (String link : socialMediaLinks) {
-            System.out.println("- " + link);
-        }
+        this.movies = movies != null ? movies : new ArrayList<>();
+        this.awards = awards != null ? awards : new ArrayList<>();
+        this.socialMediaLinks = socialMediaLinks != null ? socialMediaLinks : new ArrayList<>();
     }
 
     
     public void displayDetails() {
+        System.out.println("--- Cast Details ---");
         System.out.println("Name: " + firstName + " " + lastName);
         System.out.println("Date of Birth: " + dateOfBirth);
         System.out.println("Gender: " + gender);
         System.out.println("Nationality: " + nationality);
-        displayMovies();
-        displayAwards();
-        displaySocialMediaLinks();
+        System.out.println("Movies: " + movies);
+        System.out.println("Awards: " + awards);
+        System.out.println("Social Media Links: " + socialMediaLinks);
     }
 
-    
+   
+    public int calculatePopularityScore() {
+        int score = 0;
+        score += movies.size() * 10;  
+        score += awards.size() * 20; s
+        score += socialMediaLinks.size() * 5;
+        return score;
+    }
+
+   
     public static void main(String[] args) {
-        Cast castMember = new Cast("Mohamed", "Yasser", "22/7/2005", "Male", "Egyptian");
-        castMember.addMovie("Inspirational Journey");
-        castMember.addMovie("Code of Honor");
-        castMember.addAward("Best Rising Star Award");
-        castMember.addSocialMediaLink("https://twitter.com/mohamedyasser");
+
+        Cast castMember = new Cast(
+                "Mohamed", "Yasser",
+                "22/07/2005", "Male",
+                "Egyptian",
+                Arrays.asList("Inception", "Interstellar"),
+                Arrays.asList("Best Actor 2021", "Lifetime Achievement 2023"),
+                Arrays.asList("Facebook: fb.com/mohamed", "Twitter: twitter.com/mohamed")
+        );
+
+        
         castMember.displayDetails();
+
+        
+        int popularityScore = castMember.calculatePopularityScore();
+        System.out.println("\nPopularity Score: " + popularityScore);
     }
 }
