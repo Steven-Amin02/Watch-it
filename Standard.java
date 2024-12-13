29f049c9aa2b0eab378856b9cdc310c447d31be2
KEROLOS AYMAN EBRAHIM ABDELMALEK <149635997+keorls209@users.noreply.github.com>
2024-12-13 15:14:07 +0200

    Delete Standard.java
    l

--- Standard.java
+++
@@ -1,9 +0,0 @@
-public class Standard extends Plan
-{
-
-    public Standard() {
-        super("standard",10, 20.0, "HD", 10, 2, "Access to a larger library, Get 10% off every third movie");
-    }
-}
-
-
